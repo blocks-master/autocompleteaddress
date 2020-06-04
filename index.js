9b6b4bbd9d5fad95e96482fa9995f7901d0ba06a
Lucas Lu <longlu@ymail.com>
2020-06-03 23:31:17 -0500

    add credit card validator

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 const express = require("express");
-const routes = require("./routes/index");
+
 const path = require("path");
 
 const app = express();
+const formRouter = require("./routes/index");
 
 app.use(express.static(path.join(__dirname, "public")));
-
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-app.use("/", routes);
+// app.use("/", routes);
+app.use(formRouter);
+
 let port = process.env.PORT || process.env.IP || 3000;
 app.listen(port, function() {
 	console.log("Server Has Started!");
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,29 @@
 const express = require("express");
+const { check, validationResult } = require("express-validator");
+let creditcard = require("creditcard.js");
 
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-	res.render("auto-complete");
+	res.render("auto-complete", { creditError: '' });
 });
 
+// router.post("/", [ check("credit").isCreditCard().withMessage("Must provide a valid credit card") ], (req, res) => {
+// 	const errors = validationResult(req);
+// 	if (!errors.isEmpty()) {
+// 		return res.status(422).json({ errors: errors.array() });
+// 	}
+// 	const credit = req.body.credit;
+// });
+
+router.post("/", (req, res) => {
+	const credit = req.body.credit;
+	let obj = new creditcard();
+	if (obj.isValid(credit)) {
+		res.render("auto-complete", { creditError: '' });
+	}
+	else {
+		res.render("auto-complete", { creditError: "Invalid credit card number" });
+	}
+});
 module.exports = router;
